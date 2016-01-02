52d9e8ae4b2caff915a9b0684f2dc7aff1036c81
Jonathan Holvey <jonathan.holvey@outlook.com>
2016-01-02 16:34:02 +0000

    Modify header links so they can centre themselves dynamically

--- header.php
+++ header.php
@@ -1,14 +1,18 @@
 <div class="container">
 	<div class="header">
 		<div class="nav">
-			<div id="link_home"><a href="/">Home</a></div>
-			<div id="link_paintings"><a href="gallery/paintings">Paintings</a></div>
-			<div id="link_illustrations"><a href="gallery/illustrations">Illustrations</a></div>
-			<div id="link_prints"><a href="prints">Prints</a></div>
-			<div id="link_follow"><a href="follow">Follow</a></div>
-			<div id="link_blog"><a href="blog">Blog</a></div>
-			<div id="link_about"><a href="about">About</a></div>
-			<div id="link_contact"><a href="contact">Contact</a></div>
+			<div class="link-container">
+				<ul class="nav-links">
+					<li id="link_home"><a href="/">Home</a></li>
+					<li id="link_paintings"><a href="gallery/paintings">Paintings</a></li>
+					<li id="link_illustrations"><a href="gallery/illustrations">Illustrations</a></li>
+					<li id="link_prints"><a href="prints">Prints</a></li>
+					<li id="link_follow"><a href="follow">Follow</a></li>
+					<li id="link_blog"><a href="blog">Blog</a></li>
+					<li id="link_about"><a href="about">About</a></li>
+					<li id="link_contact"><a href="contact">Contact</a></li>
+				</ul>
+			</div>
 		</div>
 		<div class="logo">
 			<a href="/"><img src="images/logo.png" alt="Christina Holvey"/></a>
